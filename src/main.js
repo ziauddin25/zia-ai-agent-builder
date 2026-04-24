6713fd14abe09b1cd71307ca245e4d17e3d7b84b
Zia Uddin <ziauddinzoy@gmail.com>
2026-04-25 04:04:31 +0600

    delete duplicate

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
